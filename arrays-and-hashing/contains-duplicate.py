6ab7fa651dae0fbe7a24b9ae95fc58ad65d60862
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-07-19 15:15:04 +0200

    create solution to contains-duplicate - use set // exit early

--- arrays-and-hashing/contains-duplicate.py
+++ arrays-and-hashing/contains-duplicate.py
@@ -9,3 +9,28 @@ class Solution:
         #   space occupied by set.
         #
         return len(nums) != len(set(nums))
+
+#
+# #
+# # # ALTERNATIVE SOLUTION
+# #
+#
+#
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         # Observe each value and add to a set to record observations.
+#         # If a value is in the set, then it is a duplicate.
+#         # time complexity:
+#         #   O(n) where n is the number of elements in nums.
+#         #   time taken to construct a set from a list.
+#         # space complexity:
+#         #   O(n)
+#         #   space occupied by set.
+#         #
+#         obs_set = set()
+#         for n in nums:
+#             if n in obs_set:
+#                 return True
+#             else:
+#                 obs_set.add(n)
+#         return False
